c42cf860aafb03d4f8166982e98688d83532d837
ScxttM <alan.montesinos@uabc.edu.mx>
2022-03-26 02:39:09 -0700

    add changes

--- 04 - JS/practica/pokedex/app.js
+++ 04 - JS/practica/pokedex/app.js
@@ -1,6 +1,6 @@
 const fetchPokemon = () => {
-    let name = document.getElementById("name").value;
-    const url = `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`;
+    let id = document.getElementById("id").value;
+    const url = `https://pokeapi.co/api/v2/pokemon/${id.toLowerCase()}`;
     fetch(url).then((res) => {
         if (res.status != "200") {
             console.log(res);
@@ -15,7 +15,40 @@ const fetchPokemon = () => {
             console.log(data);
             let img = data.sprites.other.dream_world.front_default;
             getImage(img);
-            console.log(img);   
+            console.log(img);
+
+            let nameid = `#${data.id.toString().padStart(3, 0)} - ${data.name}`;
+            document.getElementById("card-name").innerText = nameid;
+
+            let typesObj = data.types;
+            let types = [];
+            typesObj.forEach(function (type) {
+                types.push(type['type']['name']);
+            })
+            types = types.join(" / ");
+            console.log(types);
+            document.getElementById("types").innerText = `Type: ${types}`;
+
+            document.getElementById("stats").innerHTML = "";
+            let statsArray = data.stats;
+            statsArray.forEach(function (stats) {
+                let div = document.createElement("div");
+                div.innerText = stats['stat']['name'] + "\n" + stats['base_stat'].toString();
+                document.getElementById("stats").append(div);
+                console.log(stats['stat']['name'] + " " + stats['base_stat'].toString());
+            })
+            document.getElementById("abilities").innerHTML = "";
+            list = document.createElement("ul");
+            let abilitiesArray = data.abilities;
+            abilitiesArray.forEach(function (abilities) {
+                let li = document.createElement("li");
+                li.innerText = abilities['ability']['name'];
+                list.appendChild(li);
+            })
+            h1 = document.createElement("h1");
+            h1.innerText = "Abilities: ";
+            document.getElementById("abilities").appendChild(h1);
+            document.getElementById("abilities").appendChild(list);
         }
     });
 }
